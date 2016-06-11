ad8d4eaf4d262e9a105572712f8b4c08577cec12
Alistair <plantonly@gmail.com>
2016-06-11 11:55:41 -0700

    injecting api constant into controller instead of service

--- scripts/controllers/search_controller.js
+++ scripts/controllers/search_controller.js
@@ -2,21 +2,30 @@
   'use strict';
 
   angular.module("rentCars")
-  .controller("searchController", function($scope, searchSerice) {
+  .controller("searchController", function($scope, searchSerice, apiConstant) {
 
     $scope.searchInput = {};
-    $scope.searchInput.dest = "LAX";
-    $scope.searchInput.startdate = "06/11/2016";
-    $scope.searchInput.enddate = "06/12/2016";
-    $scope.searchInput.pickuptime = "12:00";
-    $scope.searchInput.dropofftime = "12:00";
+
+    $scope.searchRentalCars = searchRentalCars;
+
+    function searchInputDefaults() {
+      $scope.searchInput.dest = "";
+      $scope.searchInput.startdate = "06/11/2016";
+      $scope.searchInput.enddate = "06/12/2016";
+      $scope.searchInput.pickuptime = "12:00";
+      $scope.searchInput.dropofftime = "12:00";
+    }
 
     activate();
 
     function activate() {
+      searchInputDefaults();
+    }
+
+    function searchRentalCars() {
       var searchInput = $scope.searchInput;
 
-      searchSerice.getCarRentals(searchInput).then(function successHandler(response) {
+      searchSerice.getCarRentals(searchInput, apiConstant).then(function successHandler(response) {
         console.log(response)
 
       }, epicFail);
--- scripts/services/search_service.js
+++ scripts/services/search_service.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module("rentCars")
-  .service("searchSerice", function($q, $http, apiConstant, $httpParamSerializer) {
+  .service("searchSerice", function($q, $http, $httpParamSerializer) {
 
-    this.getCarRentals = function(searchInput) {
+    this.getCarRentals = function(searchInput, apiConstant) {
       var deferred = $q.defer();
       var url = "http://api.hotwire.com/v1/search/car?";
 
