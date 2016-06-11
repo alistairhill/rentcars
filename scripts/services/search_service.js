64594b2cd2c7b4519155dd0cc7a68897fbaed9bc
Alistair <plantonly@gmail.com>
2016-06-11 10:29:23 -0700

    putting api constants into constant file and ignoring it

--- scripts/services/search_service.js
+++ scripts/services/search_service.js
@@ -2,11 +2,14 @@
   'use strict';
 
   angular.module("rentCars")
-  .service("searchSerice", function($q, $http) {
+  .service("searchSerice", function($q, $http, apiConstant, $httpParamSerializer) {
 
     this.getCarRentals = function() {
       var deferred = $q.defer();
-      var url = "http://api.hotwire.com/v1/search/car?apikey=x5xhvsgm3tzf2s7gh7qm8y6e&dest=LAX&startdate=01/20/2017&enddate=01/23/2017&pickuptime=10:00&dropofftime=13:30&format=jsonp&callback=JSON_CALLBACK";
+      var url = "http://api.hotwire.com/v1/search/car?&dest=LAX&startdate=01/20/2017&enddate=01/23/2017&pickuptime=10:00&dropofftime=13:30";
+
+      apiConstant = $httpParamSerializer(apiConstant);
+      url = (url + "&" + apiConstant);
 
       $http.jsonp(url)
         .success(function(data) {
