29a4961a45d0a971a03a8b8ac115bcfb50cbd012
Alistair <plantonly@gmail.com>
2016-06-11 14:26:18 -0700

    adding factory to parse results for adding car type info

--- scripts/controllers/search_controller.js
+++ scripts/controllers/search_controller.js
@@ -2,9 +2,10 @@
   'use strict';
 
   angular.module("rentCars")
-  .controller("searchController", function($scope, searchSerice, apiConstant) {
+  .controller("searchController", function($scope, searchSerice, apiConstant, parseResults) {
 
     $scope.searchInput = {};
+    $scope.carResults = [];
 
     $scope.searchRentalCars = searchRentalCars;
 
@@ -12,7 +13,7 @@
       $scope.searchInput.dest = "";
       $scope.searchInput.startdate = "06/11/2016";
       $scope.searchInput.enddate = "06/12/2016";
-      $scope.searchInput.pickuptime = "12:00";
+      $scope.searchInput.pickuptime = "16:00";
       $scope.searchInput.dropofftime = "12:00";
     }
 
@@ -24,9 +25,13 @@
 
     function searchRentalCars() {
       var searchInput = $scope.searchInput;
-
       searchSerice.getCarRentals(searchInput, apiConstant).then(function successHandler(response) {
-        console.log(response)
+        if (response.StatusDesc === "success") {
+          $scope.carResults = parseResults.addCarType(response);
+
+        } else {
+          console.log(response.Errors)
+        }
 
       }, epicFail);
     }
---
+++ scripts/factories/parse_results_factory.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular.module("rentCars")
+  .factory("parseResults", function($filter){
+    return {
+      addCarType: function(data) {
+        var results = data.Result;
+        var carTypes = data.MetaData.CarMetaData.CarTypes;
+
+        if (carTypes && results) {
+          for (var i = 0; i < results.length; i++) {
+
+            // pop filtered match from car types
+            var carType = $filter("filter")(carTypes, results[i].CarTypeCode).pop();
+
+            // add car type attributes to each rental car result
+            results[i].CarTypeName = carType.CarTypeName;
+            results[i].PossibleFeatures = carType.PossibleFeatures;
+            results[i].PossibleModels = carType.PossibleModels;
+            results[i].TypicalSeating = carType.TypicalSeating;
+          }
+
+        } else {
+          return data.Result;
+        }
+        return results;
+      }
+    }
+  });
+})();
